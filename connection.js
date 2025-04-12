d3761aabf338806cb1aeb0486a8d6c2a108141f3
Finnmeowtons <soriano.christianjose.m@gmail.com>
2025-04-12 23:45:25 +0800

    wasd

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Mais-2025-IoT",
     database: "mais",
     waitForConnections: true,
     connectionLimit: 10,
